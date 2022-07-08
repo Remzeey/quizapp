88e80a4cda3c8531e66105e5d9033440a7c2caa3
Wisdom Iruemiobe <remzwisdom@gmail.com>
2022-07-08 12:02:40 +0100

    first

---
+++ my-project/src/questions.js
@@ -0,0 +1,52 @@
+export const questions = [
+    {
+      text: "Who in 1786 invented the gold leaf electro scope?",
+      options: [
+        { id: 0, text: "Thomas Edison", isCorrect: false },
+        { id: 1, text: "Michael faraday", isCorrect: false },
+        { id: 2, text: "Isaac Newton", isCorrect: false },
+        { id: 3, text: "Abraham Bennet", isCorrect: true },
+      ],
+      difficulty: "Medium"
+    },
+    {
+      text: "Who was the first person to design models of flying machine?",
+      options: [
+        { id: 0, text: "Leonardo da Vinci", isCorrect: true },
+        { id: 1, text: "Nikola Tesla", isCorrect: false },
+        { id: 2, text: "John Newman", isCorrect: false },
+        { id: 3, text: "Andrew Scott", isCorrect: false },
+      ],
+      difficulty: "Medium"
+    },
+    {
+      text: "How many teeth does an adult human have?",
+      options: [
+        { id: 0, text: "32", isCorrect: true },
+        { id: 1, text: "36", isCorrect: false },
+        { id: 2, text: "30", isCorrect: false },
+        { id: 3, text: "34", isCorrect: false },
+      ],
+      difficulty: "Easy"
+    },
+    {
+      text: "What is the biggest planet in our solar system?",
+      options: [
+        { id: 0, text: "Mars", isCorrect: false },
+        { id: 1, text: "Jupiter", isCorrect: true },
+        { id: 2, text: "Saturn", isCorrect: false },
+        { id: 3, text: "Mercury", isCorrect: false },
+      ],
+      difficulty: "Easy"
+    },
+    {
+      text: "What is the hardest natural substance on Earth?",
+      options: [
+        { id: 0, text: "krypton", isCorrect: false },
+        { id: 1, text: "Diamond", isCorrect: true },
+        { id: 2, text: "Uranium", isCorrect: false },
+        { id: 3, text: "Brass", isCorrect: false },
+      ],
+      difficulty: "Easy"
+    },
+  ];
